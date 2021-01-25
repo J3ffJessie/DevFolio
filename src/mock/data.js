1a94e44244375631d046723ece27ac5c26e3d15e
J3ffJessie <jejess6125@gmail.com>
2021-01-24 19:07:08 -0600

    Add project images/information, add profile image, add links to live source projects as well as github source code.

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,9 +17,11 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: 'I am a passionate JavaScript Developer with a love for helping others.  I am a Veteran, and as such I hold my self to a very high standard of TeamWork, Communication, and Hard Work. I enjoy every aspect of Development and building an idea into reality.',
-  paragraphTwo: 'I am currently working on a Veteran assistance web application for finding a "buddy" to communicate with when they need someone who has been in their shoes to help them clear their minds.  I am also working with my mentor Michael(@muddybootscode) on a self-help style pomodoro web application to assist in time management and mental health awareness.',
+  img: 'profile.png',
+  paragraphOne:
+    'I am a passionate JavaScript Developer with a love for helping others.  I am a Veteran, and as such I hold my self to a very high standard of TeamWork, Communication, and Hard Work. I enjoy every aspect of Development and building an idea into reality.',
+  paragraphTwo:
+    'I am currently working on a Veteran assistance web application for finding a "buddy" to communicate with when they need someone who has been in their shoes to help them clear their minds.  I am also working with my mentor Michael(@muddybootscode) on a self-help style pomodoro web application to assist in time management and mental health awareness.',
   paragraphThree: '',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
@@ -28,21 +30,25 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'BlogImage.jpg',
+    title: 'J3 Dev Blog',
+    info:
+      'My Personal Blog that houses my written articles.  It is a holding ground for all of my published articles on Dev.to.',
+    info2:
+      'Built with Gatsby, React and utilizing the Dev.to API with GraphQL queries. I am able to automatically update my blog with my most recent Dev.to articles as soon as they are published to the Dev platform.',
+    url: 'https://j3devblog.netlify.app/',
+    repo: 'https://github.com/J3ffJessie/J3-Dev-Blog', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio',  //if no repo, the button will not show up
+    img: 'ToDo.jpg',
+    title: 'To-Do List',
+    info:
+      'I wanted to utilize NodeJS and figured a ToDo List would be a fairly easy project to build out.  Utilizing NodeJS,  Express and MongoDB I was able to create a simple web application that my wife and I can use for around the house tasks. I deployed this using Heroku to understand how that was implemented and to go through that process for future knoweldge.',
+    info2:
+      'Unfortunately do to unforseen circumstances (Apple Notes/Family Sharing) my To-Do list became boring to use and more of a hassle for my wife.  So, now it gets minimal use when I am using it to track project progress or something of that sort.',
+    url: 'https://j3-todo-app.herokuapp.com/',
+    repo: 'https://github.com/J3ffJessie/To-Do-List', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
