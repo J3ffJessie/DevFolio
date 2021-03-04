e527c3f7dd611d184251d8cd231763578e2798ec
j3ffjessie <jejess6125@gmail.com>
2021-03-03 18:19:56 -0600

    Functioning and styled contact area working. Still need to add animation to hero section to break up the plain look

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,27 +8,26 @@ import emailjs from 'emailjs-com';
 export default function ContactUs() {
 
   function sendEmail(e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     emailjs.sendForm('profile_contact', 'contact_form', e.target, 'user_lZ35CmCgyGgFIqGgPeWoZ')
       .then((result) => {
-          console.log(result.text);
       }, (error) => {
-          console.log(error.text);
       });
+      alert("Thank you for contacting me. I will reply as soon as possible.")
   }
 
   return (
-    <details>
+    <details id="contact">
       <summary>Contact Me</summary>
     <form className="contact-form" onSubmit={sendEmail}>
       <input type="hidden" name="contact_number" />
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="user_name" required="true" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" required="true" />
       <label>Message</label>
-      <textarea name="message" />
+      <textarea name="message" required="true" />
       <input type="submit" value="Send" />
     </form>
     </details>
